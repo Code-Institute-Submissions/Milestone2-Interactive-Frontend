ffccab771763774c292f7b7145919989984008bc
poldi2018 <greenion@gmx.de>
2019-04-08 21:16:42 +0000

    implememted fieldInit function

--- assets/js/memorygame.js
+++ assets/js/memorygame.js
@@ -2,5 +2,14 @@
 
 $(document).ready(function() {
 
-   
+   function fieldInit(num) {
+        var playFieldSize = num;
+        $("#playfield").empty();
+        
+        for (var i = 0; i < playFieldSize; i++) {
+            $("#playfield").append("<div class='cardshell'></div>");
+        }
+        $(".cardshell").append("<div class='card front vhalign'>?</div>");
+        $(".cardshell").append("<div class='card back vhalign'>Hi</div>");
+    }
 });
