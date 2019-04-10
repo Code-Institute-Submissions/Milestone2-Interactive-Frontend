fcc902a6828ea68ac8392c80de43cbd099f50cec
poldi2018 <greenion@gmx.de>
2019-04-10 20:59:21 +0000

    changed bg-fieldsizeBtn to class

--- assets/js/memorygame.js
+++ assets/js/memorygame.js
@@ -4,7 +4,8 @@ $(document).ready(function() {
 
     function fieldInit(num) {
         var playFieldSize = num;
-        $("#playfield").empty(); // deleting all elements from playfield container
+        $("#playfield").css("opacity", "0.0");
+        $("#playfield").delay(1000).empty(); // deleting all elements from playfield container
         for (var i = 0; i < playFieldSize; i++) {
             if (playFieldSize == 9 && i == 4) {
                 $("#playfield").append("<div id='dummycardshell'></div>");
@@ -15,13 +16,17 @@ $(document).ready(function() {
         }
         $(".cardshell").append("<div class='card front vhalign'>?</div>");
         $(".cardshell").append("<div class='card back vhalign'>Hi</div>");
-        if (playFieldSize == 9) { $("#field3x3Btn").addClass("selectedSize"); } // indicator for selected size
+        if (playFieldSize == 9) { $("#field3x3Btn").addClass("selectedSize");
+        $("#field4x4Btn").addClass("bg-fieldSizeBtn");
+        $("#field6x6Btn").addClass("bg-fieldSizeBtn");
+        } // indicator for selected size
         if (playFieldSize == 16) {
             $(".cardshell").css("width", "23.7%").css("height", "23.7%");
         }
         if (playFieldSize == 36) {
             $(".cardshell").css("width", "15.5%").css("height", "15.5%");
         }
+         $("#playfield").css("opacity", "1.0");
     }
 
     fieldInit(9); // generating playfield of 3x3 per default 
@@ -34,24 +39,25 @@ $(document).ready(function() {
     // click actions for playfield size buttons
 
     $("#field3x3Btn").click(function() {
-        $("#field3x3Btn").addClass("selectedSize"); // indicator for selected size
-        $("#field4x4Btn").removeClass("selectedSize"); // removing selected size indicator class
-        $("#field6x6Btn").removeClass("selectedSize"); // removing selected size indicator class
+        $("#field3x3Btn").addClass("selectedSize").removeClass("bg-fieldSizeBtn"); // indicator for selected size
+        $("#field4x4Btn").removeClass("selectedSize").addClass("bg-fieldSizeBtn"); // removing selected size indicator class
+        $("#field6x6Btn").removeClass("selectedSize").addClass("bg-fieldSizeBtn"); // removing selected size indicator class
         fieldInit(9); // initalizing fieldsize 3x3 cards
     });
 
 
     $("#field4x4Btn").click(function() {
-        $("#field3x3Btn").removeClass("selectedSize"); // removing selected size indicator class
-        $("#field4x4Btn").addClass("selectedSize"); // indicator for selected size
-        $("#field6x6Btn").removeClass("selectedSize"); // removing selected size indicator class
+        $("#field3x3Btn").removeClass("selectedSize").addClass("bg-fieldSizeBtn"); // removing selected size indicator class
+        $("#field4x4Btn").addClass("selectedSize").removeClass("bg-fieldSizeBtn"); // indicator for selected size
+        $("#field6x6Btn").removeClass("selectedSize").addClass("bg-fieldSizeBtn"); // removing selected size indicator class
         fieldInit(16); // initalizing fieldsize 4x4 cards
+        
     });
 
     $("#field6x6Btn").click(function() {
-        $("#field3x3Btn").removeClass("selectedSize"); // removing selected size indicator class
-        $("#field4x4Btn").removeClass("selectedSize"); // removing selected size indicator class
-        $("#field6x6Btn").addClass("selectedSize"); // indicator for selected size
+        $("#field3x3Btn").removeClass("selectedSize").addClass("bg-fieldSizeBtn"); // removing selected size indicator class
+        $("#field4x4Btn").removeClass("selectedSize").addClass("bg-fieldSizeBtn"); // removing selected size indicator class
+        $("#field6x6Btn").addClass("selectedSize").removeClass("bg-fieldSizeBtn"); // indicator for selected size
         fieldInit(36); // initalizing fieldsize 8x8 cards
     });
 
