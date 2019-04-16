a67791988fc2ec4c25bff379b0424089790a5c0d
poldi2018 <greenion@gmx.de>
2019-04-16 14:49:22 +0000

    fixed button disabling mechanism

--- assets/js/memorygame.js
+++ assets/js/memorygame.js
@@ -1,42 +1,61 @@
 /*global $*/
 
 $(document).ready(function() {
-    
+
     // predefined array with 18 pairs of cards = 36 cards max. Array will be cut to meet smaller fieldsizes.
 
-    let masterCardArray=['.card1', '.card1', '.card2', '.card2', '.card3', '.card3', '.card4', '.card4', '.card5', '.card5', '.card6', '.card6', '.card7', '.card7', '.card8', '.card8', '.card9', '.card9', '.card10', '.card10', '.card11', '.card11', '.card12', '.card12', '.card13', '.card13', '.card14', '.card14', '.card15', '.card15', '.card16', '.card16', '.card17', '.card17', '.card18', '.card18'];
-    let currentCardArray=[];
-    let takenCards=[];
-    let gameStarted= false; // indicator if game is in progress or not.
-    
+    let masterCardArray = ['.card1', '.card1', '.card2', '.card2', '.card3', '.card3', '.card4', '.card4', '.card5', '.card5', '.card6', '.card6', '.card7', '.card7', '.card8', '.card8', '.card9', '.card9', '.card10', '.card10', '.card11', '.card11', '.card12', '.card12', '.card13', '.card13', '.card14', '.card14', '.card15', '.card15', '.card16', '.card16', '.card17', '.card17', '.card18', '.card18'];
+    let currentCardArray = [];
+    let takenCards = [];
+    let gameStarted = false; // indicator if game is in progress or not.
+
     function makeBtnInactive() {
-       
-       $("#enterPlayersBtn").addClass("btnlocked").attr("id","enterPlayersBtn-locked");
-       $("#field8Btn").addClass("btnlocked").attr("id","field8Btn-locked");
-            $("#field16Btn").addClass("btnlocked").attr("id","field16Btn-locked");
-            $("#field36Btn").addClass("btnlocked").attr("id","field36Btn-locked");
-        $("#startBtn").addClass("btnlocked").attr("id","startBtn-locked");
+        $("#enterPlayersBtn").addClass("btnlocked").off("click");
+        $("#field8Btn").addClass("btnlocked").off("click");
+        $("#field16Btn").addClass("btnlocked").off("click");
+        $("#field36Btn").addClass("btnlocked").off("click");
+        $("#startBtn").addClass("btnlocked").off("click");
     }
-    
+
     function makeBtnActive() {
-       
-       $("#enterPlayersBtn-locked").removeClass("btnlocked").attr("id","enterPlayersBtn");
-       $("#field8Btn-locked").removeClass("btnlocked").attr("id","field8Btn");
-            $("#field16Btn-locked").removeClass("btnlocked").attr("id","field16Btn");
-            $("#field36Btn-locked").removeClass("btnlocked").attr("id","field36Btn");
-        $("#startBtn-locked").removeClass("btnlocked").attr("id","startBtn");
+
+        $("#enterPlayersBtn").removeClass("btnlocked").attr("id", "enterPlayersBtn");
+
+        $("#field8Btn").removeClass("btnlocked").on('click', function() {
+            $("#field8Btn").addClass("selectedSize").removeClass("bg-fieldSizeBtn"); // indicator for selected size
+            $("#field16Btn").removeClass("selectedSize").addClass("bg-fieldSizeBtn"); // removing selected size indicator class
+            $("#field36Btn").removeClass("selectedSize").addClass("bg-fieldSizeBtn"); // removing selected size indicator class
+            fieldInit(9); // initalizing fieldsize 3x3 cards / 4 pairs with one free card in the middle
+        });
+
+        $("#field16Btn").removeClass("btnlocked").on('click', function() {
+            $("#field8Btn").removeClass("selectedSize").addClass("bg-fieldSizeBtn"); // removing selected size indicator class
+            $("#field16Btn").addClass("selectedSize").removeClass("bg-fieldSizeBtn"); // indicator for selected size
+            $("#field36Btn").removeClass("selectedSize").addClass("bg-fieldSizeBtn"); // removing selected size indicator class
+            fieldInit(16); // initalizing fieldsize 4x4 cards / 8 pairs
+        });
+
+        $("#field36Btn").removeClass("btnlocked").click(function() {
+            $("#field8Btn").removeClass("selectedSize").addClass("bg-fieldSizeBtn"); // removing selected size indicator class
+            $("#field16Btn").removeClass("selectedSize").addClass("bg-fieldSizeBtn"); // removing selected size indicator class
+            $("#field36Btn").addClass("selectedSize").removeClass("bg-fieldSizeBtn"); // indicator for selected size
+            fieldInit(36); // initalizing fieldsize 6x6 cards / 18 pairs
+        });
+
+        $("#startBtn").removeClass("btnlocked").click(function() {
+            $("#scorePlayer1").text("0 points"); //for restarting game set playerscores to zero
+            $("#scorePlayer2").text("0 points"); //for restarting game set playerscores to zero
+            makeBtnInactive();
+        });
     }
-    
-    
-    function gameEngine () {
-        while (gameStarted)
-        {
-            
+
+
+    function gameEngine() {
+        while (gameStarted) {
+
         }
     }
-    
-    
-    
+
 
     function fieldInit(num) {
         var playFieldSize = num;
@@ -87,7 +106,7 @@ $(document).ready(function() {
     });
 
 
-    $("#field16Btn").click(function() {
+    $("#field16Btn").on('click', function() {
         $("#field8Btn").removeClass("selectedSize").addClass("bg-fieldSizeBtn"); // removing selected size indicator class
         $("#field16Btn").addClass("selectedSize").removeClass("bg-fieldSizeBtn"); // indicator for selected size
         $("#field36Btn").removeClass("selectedSize").addClass("bg-fieldSizeBtn"); // removing selected size indicator class
@@ -110,15 +129,13 @@ $(document).ready(function() {
     $("#startBtn").click(function() {
         $("#scorePlayer1").text("0 points"); //for restarting game set playerscores to zero
         $("#scorePlayer2").text("0 points"); //for restarting game set playerscores to zero
-    makeBtnInactive();
-        
-        
+        makeBtnInactive();
     });
 
     $("#stopBtn").click(function() {
-        
+
         makeBtnActive();
-        
+
     });
 
 });
@@ -126,4 +143,3 @@ $(document).ready(function() {
 /*
 // var fruits.splice(0, 1);        // Removes the first element of fruits
 */
-
