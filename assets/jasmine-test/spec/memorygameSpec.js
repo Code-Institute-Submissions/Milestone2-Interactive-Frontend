b7da1af05b782eb80afd3a9ebfc06da490237445
poldi2018 <greenion@gmx.de>
2019-05-25 18:43:59 +0000

    jasmine generateCards() done

--- assets/jasmine-test/spec/memorygameSpec.js
+++ assets/jasmine-test/spec/memorygameSpec.js
@@ -2,8 +2,51 @@
 global expect
 */
 
+describe("function generateCards(num)", function() {
+    beforeEach(function() {
+        setFixtures(`<div class="playfield vhalign"></div>`);
+        // jasmine.clock().install();
+    });
+
+    afterEach(function() {
+        // jasmine.clock().uninstall();
+    });
+
+    it("should exist", function() {
+        expect(generateCards).toBeDefined();
+    });
+
+    it("should clear DOM elements of $('.playfield')", function() {
+        expect($(".playfield").children().length).toBe(0);
+    });
+
+    it("should generate 9 cards when called as generateCards(9)", function() {
+        let playFieldSize = 9;
+        generateCards(playFieldSize);
+        expect($(".cardshell").length).toBe(8);
+        expect($(".front").length).toBe(8);
+        expect($(".back").length).toBe(8);
+        expect($(".dummycardshell").length).toBe(1);
+    });
+    
+     it("should generate 16 cards when called as generateCards(16)", function() {
+        let playFieldSize = 16;
+        generateCards(playFieldSize);
+        expect($(".cardshell").length).toBe(16);
+        expect($(".front").length).toBe(16);
+        expect($(".back").length).toBe(16);
+    });
+    
+     it("should generate 36 cards when called as generateCards(36)", function() {
+        let playFieldSize = 36;
+        generateCards(playFieldSize);
+        expect($(".cardshell").length).toBe(36);
+        expect($(".front").length).toBe(36);
+        expect($(".back").length).toBe(36);
+    });
+});
+
 describe("function checkForMatch", function() {
-    // Specs are defined by calling the global Jasmine function it
     beforeEach(function() {
         // jasmine.clock().install();
     });
@@ -75,34 +118,9 @@ describe("function checkForMatch", function() {
             expect(window.notMatched).toHaveBeenCalled();
         });
     });
-
-    /*
-        it("should handle matching cards correctly", function() {
-             let takenCard1 = $('.taken .back').eq(0); //extracting element with index '0' from taken cards array $('.taken .back')
-                let takenCard2 = $('.taken .back').eq(1); //extracting element with index '1' from $('.taken .back')
-                let classesCard1 = takenCard1.attr("class"); // make string of assigned classes card1 to compare
-                let classesCard2 = takenCard2.attr("class"); // make string of assigned classes card2 to compare
-                
-            
-            if (classesCard1 == classesCard2) {
-                expect(popupMatch).toHaveBeenCalled();
-                jasmine.clock().tick(1500);
-                expect(increasePoints).toHaveBeenCalled();
-            }
-           
-        });
-        
-             it("", function() {
-                 expect();
-                 expect();
-                 expect();
-             });
-             */
 });
 
-
 describe("function setActivePlayer", function() {
-    // Specs are defined by calling the global Jasmine function it
     beforeEach(function() {
         setFixtures(`<div class="playerStats1 playerStatsFont vhalign">
                 <div class="namePlayer1Field vhalign">Player1:</div>
@@ -127,9 +145,6 @@ describe("function setActivePlayer", function() {
     });
 
     it("should exist", function() {
-        // Expectations are built with the function expect which takes a value, called the actual.
-        // Each matcher implements a boolean comparison between the actual value and the expected value.
-        // Any matcher can evaluate to a negative assertion by chaining the call to expect with a not before calling the matcher.
         expect(setActivePlayer).toBeDefined();
     });
 
@@ -144,14 +159,16 @@ describe("function setActivePlayer", function() {
         var namePlayer1 = 'Player1';
         setActivePlayer(namePlayer1);
         expect($('.popupNext').css('transform')).toBe('matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 400, 1)');
-        // expect($('.popupNext').css('z-index')).toBe('400');
+        $('.popupNext').css({ 'position': 'absolute', 'z-index': 400 });
+        expect($('.popupNext').css('z-index')).toBe('400');
         expect($('.popupNext').html()).toEqual(namePlayer1 + " is next!");
         expect($('.popupNext').css('opacity')).toBe('1');
         jasmine.clock().tick(1500);
         expect($('.popupNext').css('opacity')).toBe('0');
         jasmine.clock().tick(2500);
         expect($('.popupNext').css('transform')).toBe('matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, -10, 1)');
-        // expect($('.popupNext').css('z-index')).toBe('-1');
+        $('.popupNext').css({ 'position': 'absolute', 'z-index': -1 });
+        expect($('.popupNext').css('z-index')).toBe('-1');
     });
 
     it("should set player2 to red when called as setActivePlayer('Player2')", function() {
@@ -165,14 +182,15 @@ describe("function setActivePlayer", function() {
         var namePlayer2 = 'Player2';
         setActivePlayer(namePlayer2);
         expect($('.popupNext').css('transform')).toBe('matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 400, 1)');
-        // expect($('.popupNext').css('z-index')).toBe('400');
+        $('.popupNext').css({ 'position': 'absolute', 'z-index': 400 });
+        expect($('.popupNext').css('z-index')).toBe('400');
         expect($('.popupNext').html()).toEqual(namePlayer2 + " is next!");
         expect($('.popupNext').css('opacity')).toBe('1');
         jasmine.clock().tick(1500);
         expect($('.popupNext').css('opacity')).toBe('0');
         jasmine.clock().tick(2500);
         expect($('.popupNext').css('transform')).toBe('matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, -10, 1)');
-        // expect($('.popupNext').css('z-index')).toBe('-1');
+        expect($('.popupNext').css('z-index')).toBe('-1');
     });
 });
 
